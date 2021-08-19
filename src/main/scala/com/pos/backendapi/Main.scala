17a76e075cfa6166587300e25ac8d42db9e43a95
hebzebba <kwashieseth34@gmail.com>
2021-08-19 17:25:26 +0000

    working on deployment

--- src/main/scala/com/pos/backendapi/Main.scala
+++ src/main/scala/com/pos/backendapi/Main.scala
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class Main {
-}
+class Main {}
+@SpringBootApplication
 object Main {
   def main(args : Array[String]): Unit ={
     SpringApplication.run(classOf[Main])
