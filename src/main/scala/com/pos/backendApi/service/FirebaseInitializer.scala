5755bc5bff8db80e5eb13d0bbde549386c7b463d
hebzebba <kwashieseth34@gmail.com>
2021-08-24 03:00:50 +0000

    working on deployment

--- src/main/scala/com/pos/backendApi/service/FirebaseInitializer.scala
+++ src/main/scala/com/pos/backendApi/service/FirebaseInitializer.scala
@@ -1,17 +1,22 @@
 package com.pos.backendApi.service
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.{FirebaseApp, FirebaseOptions}
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.{Resource, ResourceLoader}
 import org.springframework.stereotype.Service
 
-import java.io.{FileInputStream}
 import javax.annotation.PostConstruct
 
 @Service
-class FirebaseInitializer {
+ class FirebaseInitializer {
+
+  @Autowired
+  var resourceLoader: ResourceLoader = _
 
   @PostConstruct
   def initializer:Unit ={
-    val serviceAccount = this.getClass().getResourceAsStream("simpleposproject.json")
+    val resource : Resource = resourceLoader.getResource("classpath:simpleposproject.json")
+    val serviceAccount = resource.getInputStream()
     val options = new FirebaseOptions.Builder()
       .setCredentials(GoogleCredentials.fromStream(serviceAccount))
       .build();
